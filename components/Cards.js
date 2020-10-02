1a1e67bf97cdad8917ee97143af990010f8608fa
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-10-02 11:37:16 -0600

    Project Complete

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,102 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+function cardMaker(artObject) {
+
+        const card = document.createElement('div')
+        card.classList.add('card')
+        
+        const headline = document.createElement('div')
+        headline.classList.add('headline')
+        headline.textContent = artObject.headline
+        
+        const author = document.createElement('div')
+        author.classList.add('author')
+
+        const imgContainer = document.createElement('div')
+        imgContainer.classList.add('img-container')
+        
+        const img = document.createElement('img')
+        img.src = artObject.authorPhoto   
+
+        const authName = document.createElement('span')
+        authName.textContent = `By ${artObject.authorName}`
+
+        card.appendChild(headline)
+        card.appendChild(author)
+        author.appendChild(imgContainer)
+        imgContainer.appendChild(img)
+        author.appendChild(authName)
+       
+    card.addEventListener('click', () => {
+        console.log(artObject.headline)
+    })
+    
+    return card
+
+}
+
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(api => {
+
+        const cardEntry = document.querySelector('.cards-container')
+
+        const javascript = api.data.articles.javascript
+        const bootstrap = api.data.articles.bootstrap
+        const technology = api.data.articles.technology
+        const jquery = api.data.articles.jquery
+        const node = api.data.articles.node
+
+        javascript.forEach(object => {
+            const javaCard = cardMaker(object)
+            cardEntry.appendChild(javaCard)
+        })
+
+        bootstrap.forEach(object => {
+            const bootCard = cardMaker(object)
+            cardEntry.appendChild(bootCard)
+        })
+        technology.forEach(object => {
+            const techCard = cardMaker(object)
+            cardEntry.appendChild(techCard)
+        }) 
+        jquery.forEach(object => {
+            const jqueryCard = cardMaker(object)
+            cardEntry.appendChild(jqueryCard)
+        })   
+        node.forEach(object => {
+            const nodeCard = cardMaker(object)
+            cardEntry.appendChild(nodeCard)
+        })
+    })
+    .catch(error => {
+
+        console.log(error)
+    })
+        
+        
+        
+        
+        // const bootstrap = api.data.articles.bootstrap
+        // const technology = api.data.articles.technology
+        // const jquery = api.data.articles.jquery
+        // const node = api.data.articles.node
+
+        // const javaCard = cardMaker(javascript) 
+        // const bootCard = cardMaker(bootstrap)
+        // const techCard = cardMaker(technology)
+        // const jqCard = cardMaker(jquery)
+        // const nodeCard = cardMaker(node)
+       
+        // const cardContainer = document.querySelector('.cards-container')
+
+        // cardContainer.appendChild(javaCard)
+        // cardContainer.appendChild(bootCard)
+        // cardContainer.appendChild(techCard)
+        // cardContainer.appendChild(jqCard)
+        // cardContainer.appendChild(nodeCard)
+
+
